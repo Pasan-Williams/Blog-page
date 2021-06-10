c5a52c253dda9c4fc67a38c36e91ce8440964512
Pasan-Williams <pasan.w@evicio.com>
2021-06-10 14:01:35 +0530

    Hero part add

--- src/header/HeaderBar.js
+++ src/header/HeaderBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import {
   AppBar,
   Typography,
@@ -7,42 +6,30 @@ import {
   Toolbar,
   IconButton,
 } from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import "../header/header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import Hero from "./Hero"
 
 export default function HeaderBar() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div className="root">
       <AppBar position="static">
         <Toolbar>
           <IconButton
             edge="start"
-            className={classes.menuButton}
+            className="menuButton"
             color="inherit"
             aria-label="menu"
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" className="title">
             My Blog
           </Typography>
-          <Button color="inherit">Create New</Button>
+          <Button variant="contained">Create New</Button>
         </Toolbar>
       </AppBar>
+      <Hero/>
     </div>
   );
 }
