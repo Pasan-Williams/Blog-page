0e18553e2695944a4a17a679f57a76d93aba694c
Pasan-Williams <pasan.w@evicio.com>
2021-06-11 10:30:21 +0530

    icon alignment

--- src/header/CardsGrid.js
+++ src/header/CardsGrid.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
-import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
-import EditRoundedIcon from '@material-ui/icons/EditRounded';
+import DeleteRoundedIcon from "@material-ui/icons/DeleteRounded";
+import EditRoundedIcon from "@material-ui/icons/EditRounded";
 import {
   Container,
   Grid,
@@ -14,40 +14,46 @@ import {
   IconButton,
 } from "@material-ui/core";
 
-
 export default function CardGrid() {
   const numbers = [1, 2, 3, 4, 5, 6];
   return (
     <Container className="cardGrid">
-      <Grid container spacing={3} >
-      {numbers.map((card) => (
-        <Grid item key={card} xs={12} sm={6} md={4}>
-
-          <Card className="card">
-            <CardMedia className="cardMedia" image="./cardImage.jpg" title="Image title" />
-            <CardContent className="cardContent">
-              <Typography gutterBottom variant="h5" component="h2">
-                Heading
-              </Typography>
-              <Typography>
-                This is a media card. You can use this section to describe the
-                content.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary">
-                View
-              </Button>
-              <IconButton aria-label="Edit">
-              <EditRoundedIcon color="primary"/>
-              </IconButton>
-              <IconButton aria-label="Delete">
-              <DeleteRoundedIcon color="secondary"/>
-              </IconButton>
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
+      <Grid container spacing={3}>
+        {numbers.map((card) => (
+          <Grid item key={card} xs={12} sm={6} md={4}>
+            <Card className="card">
+              <CardMedia
+                className="cardMediaStyle"
+                Image='../Assets/images/cardImage.jpg'
+                title="Image title"
+              />
+              <CardContent className="cardContent">
+                <Typography gutterBottom variant="h5" component="h2">
+                  Heading
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  This is a media card. You can use this section to describe the
+                  content.
+                </Typography>
+              </CardContent>
+              <CardActions className="IconStyles">
+                <div>
+                <Button size="small" color="primary">
+                  View
+                </Button>
+                </div>
+                <div >
+                  <IconButton aria-label="Edit">
+                    <EditRoundedIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton aria-label="Delete">
+                    <DeleteRoundedIcon fontSize="small" />
+                  </IconButton>
+                </div>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
