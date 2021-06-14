ece22aed5bb29d80cb5d00f19d5a20a3fd5d0e99
Pasan-Williams <pasan.w@evicio.com>
2021-06-14 16:30:33 +0530

    create form

--- src/Component/CreateForm.js
+++ src/Component/CreateForm.js
@@ -16,14 +16,14 @@ import "./CreateForm.css";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
 export default function CreateForm() {
-  const initialInputState = { firstName: "", lastName: "",blogName: "" };
+  const initialInputState = { firstName: "", lastName: "", blogName: "" };
   const [eachEntry, setEachEntry] = useState(initialInputState);
   const { firstName, lastName, blogName } = eachEntry;
 
-  const handleInputChange = e => {
-    setEachEntry({...eachEntry, [e.target.name]: e.target.value });
+  const handleInputChange = (e) => {
+    setEachEntry({ ...eachEntry, [e.target.name]: e.target.value });
   };
-  const handleInputSubmit = e => {
+  const handleInputSubmit = (e) => {
     // updateLeaderBoardArray(eachEntry);
     console.log("submit");
   };
@@ -51,81 +51,86 @@ export default function CreateForm() {
             </Typography>
           </div>
         </div>
+        <form onSubmit={handleInputSubmit}>
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstName"
+                label="First Name"
+                value={firstName}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="lastName"
+                label="Last Name"
+                value={lastName}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="blogName"
+                name="blogName"
+                label="Blog Title"
+                value={blogName}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            </Grid>
+          </Grid>
 
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="firstName"
-              label="First Name"
-              value={firstName}
-              onChange={handleInputChange}
-             
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="lastName"
-              label="Last Name"
-              value={lastName}
-              onChange={handleInputChange}
-              fullWidth
-            />
-          </Grid>
           <Grid item xs={12}>
-            <TextField
-              required
-              id="blogName"
-              name="blogName"
-              label="Blog Title"
-              value={blogName}
-              onChange={handleInputChange}
-              fullWidth
+            <div className="textStyle">Blog Category :</div>
+
+            <FormControlLabel
+              value="end"
+              control={<Radio color="primary" />}
+              label="Travel"
+            />
+            <FormControlLabel
+              value="end"
+              control={<Radio color="primary" />}
+              label="Animals"
+            />
+            <FormControlLabel
+              value="end"
+              control={<Radio color="primary" />}
+              label="Music"
+            />
+            <FormControlLabel
+              value="end"
+              control={<Radio color="primary" />}
+              label="Sports"
             />
           </Grid>
-        </Grid>
-
-        <Grid item xs={12}>
-          <div className="textStyle">Blog Category :</div>
-
-          <FormControlLabel
-            value="end"
-            control={<Radio color="primary" />}
-            label="Travel"
-          />
-          <FormControlLabel
-            value="end"
-            control={<Radio color="primary" />}
-            label="Animals"
-          />
-          <FormControlLabel
-            value="end"
-            control={<Radio color="primary" />}
-            label="Music"
-          />
-          <FormControlLabel
-            value="end"
-            control={<Radio color="primary" />}
-            label="Sports"
-          />
-        </Grid>
-        <Grid>
           <Grid>
-            <TextField
-              id="standard-textarea"
-              label="Blog Description"
-              rows={10}
-              multiline
-              fullWidth
-            />
+            <Grid>
+              <TextField
+                id="standard-textarea"
+                label="Blog Description"
+                rows={10}
+                multiline
+                fullWidth
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <br></br>
-        <Grid item xs={12}>
-          <Button type="submit" variant="contained" color="primary" onClick={handleInputSubmit}>
-            Submit
-          </Button>
-        </Grid>
+          <br></br>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={handleInputSubmit}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </form>
       </Paper>
     </Container>
   );
